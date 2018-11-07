9f80b72f30b2daebafbc53be902dc4362e7435f7
Steven Mouret <steven.mouret.stratis@gmail.com>
2018-11-07 13:02:28 +0100

    Rebuild navigations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    console.log('toto');
+
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
