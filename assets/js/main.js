c6a3b4850a1d5790efc4aa035da19b8750b5ab8d
Steven Mouret <steven.mouret.stratis@gmail.com>
2020-04-03 16:20:14 +0200

    Add A2HS button

--- assets/js/main.js
+++ assets/js/main.js
@@ -128,7 +128,30 @@ const burgerMenu = () => {
     }
 };
 
+function Add2HomeScreen() {
+    let deferredPrompt;
+    const addBtn = document.querySelector('.add-button');
+    //addBtn.style.display = 'none';
 
+    window.addEventListener('beforeinstallprompt', (e) => {
+        e.preventDefault();
+        deferredPrompt = e;
+        addBtn.style.display = 'block';
+
+        addBtn.addEventListener('click', (e) => {
+            addBtn.style.display = 'none';
+            deferredPrompt.prompt();
+            deferredPrompt.userChoice.then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('User accepted the A2HS prompt');
+                } else {
+                    console.log('User dismissed the A2HS prompt');
+                }
+                deferredPrompt = null;
+            });
+        });
+    });
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     anchorHeadingLink();
@@ -137,4 +160,7 @@ document.addEventListener("DOMContentLoaded", function() {
     onChangeOnSelect();
     noOnChangeOnSelect();
     burgerMenu();
+    Add2HomeScreen();
 });
+
+
